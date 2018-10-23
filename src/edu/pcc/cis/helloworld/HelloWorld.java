bf38438d7e02d4aa9e93f3225ee4af08a60b0819
Nozomi Daniel <nozomi.daniel@pcc.edu>
2018-10-22 19:21:33 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Nozomi Daniel on 2018.10.22
  **/
 public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello, World!");
+    }
 }
